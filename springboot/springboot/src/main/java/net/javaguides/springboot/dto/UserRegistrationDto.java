75399dd495e7924106278a7403a0fd50f3807831
Aquarezia Parakash Koirala <104826956+Aquareziaprakash@users.noreply.github.com>
2024-08-10 10:25:05 +0545

    Add files via upload

---
+++ springboot/springboot/src/main/java/net/javaguides/springboot/dto/UserRegistrationDto.java
@@ -0,0 +1,55 @@
+package net.javaguides.springboot.dto;
+
+public class UserRegistrationDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public UserRegistrationDto() {
+    }
+
+    public UserRegistrationDto(String firstName, String lastName, String email, String password) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getter and Setter for firstName
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // Getter and Setter for lastName
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // Getter and Setter for email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter and Setter for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ springboot/springboot/src/main/java/net/javaguides/springboot/model/Role.java
@@ -0,0 +1,41 @@
+package net.javaguides.springboot.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    // Default constructor
+    public Role() {
+    }
+
+    // Constructor with parameters
+    public Role(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
